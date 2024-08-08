f1ab1c58ad15a3b00963ad4d4ce6d37fb67ebd3d
Tmdimperiodigital <animeskawaki@gmail.com>
2024-08-08 15:52:48 -0300

    Subi meus arquivos js aqui

---
+++ js/preloader.js
@@ -0,0 +1,29 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.addEventListener('DOMContentLoaded', function() {
+    QueryLoader2(document.querySelector("body"), {
+        backgroundColor: "#000",
+        percentage: true,
+        barHeight: 1,
+        minimumTime: 200,
+        fadeOutTime: 1000,
+        onComplete() {
+            $('.logo').addClass('logoAnimation');
+            $('.path').addClass('pathAnimation');
+        }
+    });
+
+    
+});
+
+}
